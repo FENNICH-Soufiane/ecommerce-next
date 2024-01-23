18254f2941a1aab8186475d1783250ae8b296d26
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-01-23 21:58:51 +0100

    login_with_next_auth_&_google

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import "@/styles/globals.css";
+import { Poppins } from "next/font/google";
+
+const inter = Poppins({ subsets: ["latin"], weight: "400" });
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <main className={`${inter.className}`}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </main>
+  );
 }
